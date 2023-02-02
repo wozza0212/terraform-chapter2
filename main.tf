b215ec0709544d1954bd5ea49816232f5cf088ef
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-02 20:58:28 +0000

    chang terraform state to terraform_state_2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+resource "aws_s3_bucket" "terraform_state_2" {
     bucket = "terraform-up-and-running-state"
 
     # prevent accidental deletion of this bucket 
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "terraform_state" {
 #enable versioning of state files
 
 resource "aws_s3_bucket_versioning" "enabled" {
-    bucket = aws_s3_bucket.terraform_state.id
+    bucket = aws_s3_bucket.terraform_state_2.id
     versioning_configuration {
         status = "Enabled"
     }
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_versioning" "enabled" {
 
 # enable server-side encryption by default
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-    bucket = aws_s3_bucket.terraform_state.id
+    bucket = aws_s3_bucket.terraform_state_2.id
 
     rule {
         apply_server_side_encryption_by_default {
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 
 # explicitly block all public access
 resource "aws_s3_bucket_public_access_block" "public_access" {
-    bucket = aws_s3_bucket.terraform_state.id
+    bucket = aws_s3_bucket.terraform_state_2.id
     block_public_acls = true
     block_public_policy = true
     ignore_public_acls = true
