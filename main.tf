545f4f253f62b8f0c354c08f68a2d37547b972f1
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-02 20:43:13 +0000

    added dynamodb table for locking

--- main.tf
+++ main.tf
@@ -30,3 +30,23 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
         }
     }
 }
+
+# explicitly block all public access
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.terraform_state.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform_up_and_running_locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
